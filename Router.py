12d156a843978b1831695c273d3b1c89894f51d8
Bayun <i.korniyenko@list.ru>
2019-09-06 18:43:15 +0300

    fixed router

--- Router.py
+++ Router.py
@@ -17,21 +17,22 @@ class Router:
             return lambda path: self.request(attribute.upper(), path)
         raise AttributeError(f" instance has not {attribute} attribute")
 
-    def add_path(self, path, attribute, info):
-        list_of_data = path, attribute, info
+    def add_path(self, path, attribute, func):
+        list_of_data = path, attribute, func
         database = self.database.append(list_of_data)
         return f"ok, path - \"{path}\", attribute - " \
             f"\"{attribute}\" was include"
-
-    def my_info(self, attribute, path):
-        return 200, {"me": "Joanne"}
 
     def request(self, method, path):
         if path not in ([(el[0]) for el in router.database]):
             return f"Error 404: {path} not found"
         elif method not in self.METHODS:
             return f"Error 405: Method{method} not Allowed"
-        return router.my_info(method, path)
+        return my_info(method, path)
+
+
+def my_info(path, method):
+    return 200, {"me": "Joanne"}
 
 
 def get(self, path=None):
@@ -41,9 +42,11 @@ def get(self, path=None):
 
 if __name__ == "__main__":
     router = Router()
-    print(router.add_path("/av", "GET", "cancan"))
-    print(router.add_path("/am", "GIVEN", "name"))
+    print(router.add_path("/av", "GET", my_info))
+    print(router.add_path("/am", "GIVEN", my_info))
+    print(router.add_path("/am", "POST", my_info))
     print(router.get("/am"))
+    print(router.delete("/am"))
     print(router.request('GET', '/me'))
     print(router.get("/us"))
     # print(router.prg("/am"))
